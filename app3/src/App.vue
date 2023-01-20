229e5823b5eac3cc1372c69ada1b05994bcca567
Conradkry <ckrytusa@eagles.bridgewater.edu>
2023-01-20 03:12:24 -0500

    uploading app3

---
+++ app3/src/App.vue
@@ -0,0 +1,12 @@
+
+<script>
+ import Life from './components/Life.vue'
+export default {
+  components: {
+    Life
+  }
+}
+</script>
+<template>
+  <Life></Life>
+</template>
---
+++ app3/src/components/Life.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="life">
+    <div id="title">Death</div>
+    <img alt="tombstone" src="../assets/Life.jpeg" />
+    <div id="caption">gives life meaning</div>
+  </div>
+</template>
+
+<style>
+#life {
+  aspect-ratio: 1 / 1;
+  width: 200pt;
+  background-color:rgb(49, 49, 49);
+  border-radius: 9px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+#title {
+  font-size: 25pt;
+  color: white;
+}
+#caption {
+  font-size: 15pt;
+  color: white;
+}
+img {
+  display: inline-block;
+  width: 50%;
+}
+</style>
